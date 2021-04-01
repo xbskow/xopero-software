ba9f9553a3b9a5d68352b9ac68e41fd6d11f50b0
xbskow <skowron.b00@gmail.com>
2021-04-01 18:14:33 +0200

    One WPF and one console app programmed.

---
+++ c-sharp-projects/strona125/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+
+namespace strona125
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void changeText_Click(object sender, RoutedEventArgs e)
+        {
+            if (enableCheckBox.IsChecked == true)
+            {
+                if (labelToChange.HorizontalAlignment != HorizontalAlignment.Left)
+                {
+                    labelToChange.HorizontalAlignment = HorizontalAlignment.Left;
+                    labelToChange.Text = "Z lewej";
+                }
+                else if (labelToChange.HorizontalAlignment != HorizontalAlignment.Right)
+                {
+                    labelToChange.HorizontalAlignment = HorizontalAlignment.Right;
+                    labelToChange.Text = "Z prawej";
+                }
+            }
+            else
+            {
+                labelToChange.Text = "Możliwość zmiany tekstu została wyłączona";
+                labelToChange.HorizontalAlignment = HorizontalAlignment.Center;
+            }
+            
+        }
+    }
+}
---
+++ c-sharp-projects/strona126/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace strona126
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            string poem = "";
+
+            while (x < 4)
+            {
+                poem += "a";
+                if (x < 1)
+                {
+                    poem += " ";
+                }
+                poem += "n";
+
+                if (x > 1)
+                {
+                    poem += " oyster";
+                    x += 2;
+                }
+                if (x == 1)
+                {
+                    poem += "noys ";
+                }
+                if (x < 1)
+                {
+                    poem += "oise ";
+                }
+                x++;
+            }
+            Console.WriteLine(poem);
+        }
+    }
+}
