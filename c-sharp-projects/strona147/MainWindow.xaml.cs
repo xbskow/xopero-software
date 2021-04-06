a4ffa5130ccc9dd88240528103213774809471d0
xbskow <skowron.b00@gmail.com>
2021-04-06 12:42:02 +0200

    did a task from page 147

---
+++ c-sharp-projects/strona147/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace strona147
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void submitButton_Click(object sender, RoutedEventArgs e)
+        {
+            int len = Talker.BlahBlahBlah(stringText.Text, int.Parse(intText.Text));
+            MessageBox.Show($"Długość wiadomości to: {len}");
+        }
+    }
+}
