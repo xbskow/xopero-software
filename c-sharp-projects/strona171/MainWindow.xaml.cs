f5a2599593b641c80bf29310a747942404c51cd9
xbskow <skowron.b00@gmail.com>
2021-04-06 15:24:53 +0200

    tasks from pages 171-178 done

---
+++ c-sharp-projects/strona171/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace strona171
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Guy joe;
+        Guy bob;
+        int bank = 100;
+        
+        public void UpdateForm()
+        {
+            joesCashLabel.Text = $"{joe.Name} ma {joe.Cash} zł";
+            bobsCashLabel.Text = $"{bob.Name} ma {bob.Cash} zł";
+            bankCashLabel.Text = $"Bank ma {bank} zł";
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            joe = new Guy() { Name = "Joe", Cash = 50 };
+            bob = new Guy() { Name = "Bob", Cash = 100 };
+
+            UpdateForm();
+        }
+
+        private void bankGivesToJoe_Click(object sender, RoutedEventArgs e)
+        {
+            if (bank >= 10)
+            {
+                bank -= joe.ReceiveCash(10);
+                UpdateForm();
+            }
+            else
+            {
+                MessageBox.Show("Bank nie posiada takiej ilości pieniędzy");
+            }
+        }
+
+        private void bankTakesfromBob_Click(object sender, RoutedEventArgs e)
+        {
+            bank += bob.GiveCash(5);
+            UpdateForm();
+        }
+
+        private void joeGivesToBob_Click(object sender, RoutedEventArgs e)
+        {
+            bob.ReceiveCash(joe.GiveCash(10));
+            UpdateForm();
+        }
+
+        private void bobGivesToJoe_Click(object sender, RoutedEventArgs e)
+        {
+            joe.ReceiveCash(bob.GiveCash(5));
+            UpdateForm();
+        }
+    }
+}
