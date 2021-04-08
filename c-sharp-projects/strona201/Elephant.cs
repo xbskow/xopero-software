f24f9a2bee18ca3a95d879f034ec8b544e315660
xbskow <skowron.b00@gmail.com>
2021-04-08 11:51:11 +0200

    page 201 done

---
+++ c-sharp-projects/strona201/Elephant.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace strona201
+{
+    class Elephant
+    {
+        public string Name;
+        public int EarSize;
+
+        public void WhoAmI()
+        {
+            MessageBox.Show($"Moje uszy mają {EarSize} centymetrów szerokości.", $"{Name} mówi...");
+        }
+    }
+}
---
+++ c-sharp-projects/strona201/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace strona201
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Elephant lloyd = new Elephant() { Name = "Lloyd", EarSize = 40 };
+        Elephant lucinda = new Elephant() { Name = "Lucinda", EarSize = 33 };
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void lloydBox_Click(object sender, RoutedEventArgs e)
+        {
+            lloyd.WhoAmI();
+        }
+
+        private void lucindaBox_Click(object sender, RoutedEventArgs e)
+        {
+            lucinda.WhoAmI();
+        }
+
+        private void swapBox_Click(object sender, RoutedEventArgs e)
+        {
+            Elephant lucindaBackup = new Elephant { Name = "Lucinda", EarSize = lucinda.EarSize };
+            lucinda = lloyd;
+            lloyd = lucindaBackup;
+            MessageBox.Show("Obiekty zamienione.", "Wiadomość");
+        }
+    }
+}
