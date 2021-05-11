421817dae5e910fa63fa62edf5ae690c0e515015
xbskow <skowron.b00@gmail.com>
2021-05-11 13:39:58 +0200

    leftover code from SharpAESCrypt

--- c-sharp-projects/FileOperations/Encryption.cs
+++ c-sharp-projects/FileOperations/Encryption.cs
@@ -31,24 +31,6 @@ namespace FileOperations
             {
                 return Misc.Finish(false, title, e);
             }
-            /*
-            try
-            {
-                SharpAESCrypt.SharpAESCrypt.Encrypt("8RlbaUk4", source, Path.Combine(destination, Path.GetFileName(source)));
-                if (verify)
-                {
-                    Decrypt(Path.Combine(destination, Path.GetFileName(source)), title);
-                    verification = " --- " + Checksum.CompareChecksums(source, Path.Combine(rootDir, "Decrypted", Path.GetFileName(source))); 
-                    File.Delete(Path.Combine(rootDir, "Decrypted", Path.GetFileName(source)));
-                }
-                return Misc.Finish(true, title) + verification;
-
-            }
-            catch (Exception e)
-            {
-                return Misc.Finish(false, title, e);
-            }
-            */
         }
         public static string Decrypt(string source, string title)
         {
@@ -65,17 +47,6 @@ namespace FileOperations
             {
                 return Misc.Finish(false, title, e);
             }
-            /*
-            try
-            {
-                SharpAESCrypt.SharpAESCrypt.Decrypt("8RlbaUk4", source, Path.Combine(destination, Path.GetFileName(source))); 
-                return Misc.Finish(true, title);
-            }
-            catch (Exception e)
-            {
-                return Misc.Finish(false, title, e);
-            }
-            */
         }
     }
 }
